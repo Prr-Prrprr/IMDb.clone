9e8cf722e5bc422cd0390423bfc31a13d4e7924f
MarinaM <meowm5476@gmail.com>
2022-05-06 00:18:25 +0300

    change cards and create a movie model

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,9 @@
+class MoviesController < ApplicationController
+	def index
+		@movies = Movie.all
+	end
+
+	def show
+		@movies = Movie.all
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
+  resources :movies
   # get 'home/index'
 
-  root 'home#index'
+  root 'movies#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
