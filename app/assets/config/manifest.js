8179af3ef85f5163ddcab64b6850d2122e1bacf7
MarinaM <meowm5476@gmail.com>
2022-05-10 20:34:36 +0300

    update rating

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,10 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+//= link jquery.min.js
+//= link jquery_ujs.js
+//= link popper.js
+//= link bootstrap.min.js
+
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
