6cd2f189ead26f5df2002be861b28ca2aba34ca2
MarinaM <meowm5476@gmail.com>
2022-05-11 19:16:31 +0300

    update ruby to 2.7.6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -58,13 +58,14 @@ class MoviesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_movie
-      @movie = Movie.find(params[:id])
-    end
+  
+  # Use callbacks to share common setup or constraints between actions.
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def movie_params
-      params.require(:movie).permit(:name)
-    end
+  # Only allow a list of trusted parameters through.
+  def movie_params
+    params.require(:movie).permit(:name, :text, :category)
+  end
 end
